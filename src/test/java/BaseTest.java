4eb8c3644aabb1ae1341e95e45c64c441d4e131c
Leo Booth <leobooth@users.noreply.github.com>
2023-11-24 20:10:02 -0600

    moved driver init to BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+
+  WebDriver initiateTestDriver() {
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    return driver;
+  }
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,18 +1,14 @@
 import com.leobooth.WaitUtils;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SampleTest {
+public class SampleTest extends BaseTest {
 
   @Test
   public void testPageTitle() {
-    WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = initiateTestDriver();
     driver.manage().window().maximize();
-
     driver.get("https://the-internet.herokuapp.com/");
     String expectedTitle = "The Internet";
     String actualTitle = driver.getTitle();
