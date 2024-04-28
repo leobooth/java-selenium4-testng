8a904f363528f25cc429265c9e156b5dd4cf72b0
Leo Booth <lbooth@autofi.io>
2024-04-27 21:27:51 -0500

    created and ran first version of complete test

--- src/main/java/com/leobooth/mitsubishiclickshop/pages/SearchResultsPage.java
+++ src/main/java/com/leobooth/mitsubishiclickshop/pages/SearchResultsPage.java
@@ -2,10 +2,15 @@ package com.leobooth.mitsubishiclickshop.pages;
 
 import com.leobooth.pages.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class SearchResultsPage extends BasePage {
 
@@ -49,6 +54,34 @@ public class SearchResultsPage extends BasePage {
             "//descendant::div[contains(@class,'relative')]/following-sibling::span/descendant::div[contains(@class,'flex-col')][position()=2]/descendant::p[position()=3]"
     );
 
+    By srpNoVehiclesFound = By.xpath(
+            "//h4[contains(text(),'No vehicles found')]"
+    );
+
+    public void waitUntilSrpTitleVisible(int timeoutInSeconds) {
+        WebDriverWait waitForTitle = new WebDriverWait(getDriver(), Duration.ofSeconds(timeoutInSeconds));
+        waitForTitle.until(ExpectedConditions.titleContains("Mitsubishi"));
+    }
+
+    public void waitUntilDealerNameVisible(int timeoutInSeconds) {
+        WebDriverWait waitForDealerName = new WebDriverWait(getDriver(), Duration.ofSeconds(timeoutInSeconds));
+        waitForDealerName.until(
+                ExpectedConditions.or(
+                    ExpectedConditions.textToBePresentInElementLocated(srpTileDealerNameSelector, "Mitsubishi"),
+                    ExpectedConditions.textToBePresentInElementLocated(srpTileDealerNameSelector, "MITSUBISHI"),
+                    ExpectedConditions.visibilityOfElementLocated(srpTileDealerNameSelector)
+                )
+        );
+    }
+
+    public boolean isNoVehiclesFoundVisible() {
+        try {
+            WebElement noVehiclesFoundElement = getDriver().findElement(srpNoVehiclesFound);
+            return noVehiclesFoundElement.isDisplayed();
+        } catch (NoSuchElementException | TimeoutException e) {
+            return false;
+        }
+    }
 
     public String getSRPTileDealerName(int tileNumber) {
         ArrayList<WebElement> dealerNames = new ArrayList<>(getDriver().findElements(srpTileDealerNameSelector));
--- src/test/java/com/leobooth/mitsubishiclickshop/ClickShopTest.java
+++ src/test/java/com/leobooth/mitsubishiclickshop/ClickShopTest.java
@@ -2,52 +2,87 @@ package com.leobooth.mitsubishiclickshop;
 
 import com.leobooth.BaseTest;
 import com.leobooth.mitsubishiclickshop.pages.SearchResultsPage;
-import com.leobooth.utils.WaitUtils;
+import com.leobooth.utils.CSVToDealerZipList;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ClickShopTest extends BaseTest {
 
+    private SearchResultsPage navToSrp(WebDriver driver, SoftAssert softAssert, CSVToDealerZipList.DealerZip expectedDealer) {
+        String urlVDPByZipCode = ClickShopUrl.getClickShopUatUrl(expectedDealer.getZipCode());
+
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver, urlVDPByZipCode);
+        searchResultsPage.navToPage();
+
+        searchResultsPage.waitUntilAllPageElementsVisible(15);
+        String actualPageTitle = searchResultsPage.getPageTitle();
+
+        String expectedPageTitle = "Shop new Mitsubishi cars near you and pre-qualify | Mitsubishi powered by Drive";
+        Assert.assertEquals(actualPageTitle, expectedPageTitle, "ClickShop home page title is not visible.");
+
+        return searchResultsPage;
+    }
+
+    private void assertDealerNameAndDistance(SearchResultsPage srp, SoftAssert softAssert, int tileNumber, CSVToDealerZipList.DealerZip expectedDealer, int expectedDistance) {
+        String actualDealerName = "";
+        String actualDistanceAsString = "";
+        int actualDistance = -1;
+
+        try {
+            srp.waitUntilDealerNameVisible(15);
+            actualDealerName = srp.getSRPTileDealerName(tileNumber);
+            actualDistanceAsString = srp.getSRPTileDistance(tileNumber);
+            actualDistance = Integer.parseInt(
+                    actualDistanceAsString.substring(0, actualDistanceAsString.indexOf("mi")).trim()
+            );
+        } catch (TimeoutException e) {
+            System.out.println("Unable to find dealer name: " + expectedDealer.getDealership());
+
+            if (srp.isNoVehiclesFoundVisible()) {
+                System.out.println("no vehicles found.");
+                actualDealerName = "no vehicles found";
+                actualDistance = -1;
+            } else {
+                softAssert.assertAll();
+            }
+        }
+
+        System.out.println("expected dealer: " + expectedDealer.getDealership() + ", actual dealer: " + actualDealerName);
+
+        softAssert.assertEquals(
+                actualDealerName.toUpperCase(), expectedDealer.getDealership().toUpperCase(),
+                "Unexpected dealer name: " + actualDealerName + ", ZIP Code: " + expectedDealer.getZipCode());
+        softAssert.assertEquals(actualDistance, expectedDistance, "Unexpected distance from ZIP Code " +
+            expectedDealer.getZipCode() + " for dealer " + actualDealerName + ": " + actualDistance);
+    }
+
     @Test
-    public void testClickShopTile() {
+    public void testClickShopDealers() {
         WebDriver driver = setupTestDriver(TestBrowser.CHROME);
         driver.manage().window().maximize();
         SoftAssert softAssert = new SoftAssert();
 
-        String expectedDealerName = "STROUDSBURG MITSUBISHI";
-        String zipCode = "18301";
         int expectedDistance = 1;
 
-        String urlVDPByZipCode = ClickShopUrl.getClickShopUatUrl(zipCode);
+        ArrayList<CSVToDealerZipList.DealerZip> dealerZipList = new ArrayList<>();
 
-        System.out.println(urlVDPByZipCode);
+        try {
+            dealerZipList = CSVToDealerZipList.convertCSV("/Users/lbooth/Desktop/DealershipsAndZIPCodes-20240425.csv", "," , false);
+        } catch (IOException e) {
+            Assert.fail("Unable to convert CSV to DealerZipList");
+        }
 
-        SearchResultsPage srp = new SearchResultsPage(driver, urlVDPByZipCode);
-        srp.navToPage();
-
-        WaitUtils.hardWaitForSeconds(10);
-
-        String clickShopHomePageTitleText = "Shop new Mitsubishi cars near you and pre-qualify | Mitsubishi powered by Drive";
-        String actualPageTitle = srp.getPageTitle();
-        String expectedPageTitle = clickShopHomePageTitleText;
-        System.out.println("Page title: " + actualPageTitle);
-        Assert.assertEquals(actualPageTitle, expectedPageTitle, "Clickshop home page title is not visible.");
-
-        WaitUtils.hardWaitForSeconds(3);
-
-        String actualDealerName = srp.getSRPTileDealerName(1);
-        String actualDistanceAsString = srp.getSRPTileDistance(1);
-        int actualDistance = Integer.parseInt(
-                actualDistanceAsString.substring(0, actualDistanceAsString.indexOf("mi")).trim()
-        );
-
-        System.out.println(actualDealerName);
-        System.out.println(actualDistance);
-
-        softAssert.assertEquals(actualDealerName.toUpperCase(),expectedDealerName.toUpperCase(),"Unexpected dealer name: " + actualDealerName);
-        softAssert.assertEquals(actualDistance, expectedDistance, "Unexpected distance for dealer " + actualDealerName + ": " + actualDistance);
+        //for (CSVToDealerZipList.DealerZip dealerZip : dealerZipList) {
+        for (int index = 0; index < dealerZipList.size(); index++) {
+            SearchResultsPage srp = navToSrp(driver, softAssert, dealerZipList.get(index));
+            assertDealerNameAndDistance(srp, softAssert, 1, dealerZipList.get(index), expectedDistance);
+        }
 
         softAssert.assertAll();
     }
